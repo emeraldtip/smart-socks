85df2252f9733466e0f3a4e8da9780da37435ed4
emeraldtip <emeraldtipyt@gmail.com>
2026-02-10 17:18:01 +0200

    Initial commit

---
+++ sock_goon/src/sock_goon.cpp
@@ -0,0 +1,22 @@
+#include "Arduino.h"
+#include "WiFi.h"
+
+//Mac of host is D0:CF:13:27:F0:AC
+void setup() {
+  pinMode(A10,INPUT);
+  analogSetAttenuation(ADC_11db);
+  
+  Serial.begin(115200);
+  WiFi.mode(WIFI_MODE_STA);
+}
+
+void loop() {
+  int value = analogRead(A10);
+  int spaces = value / 52;
+
+  for (int i = 0; i<spaces; i++) {
+    Serial.print(" ");
+  }
+  Serial.println(value);
+  delay(100);
+}
