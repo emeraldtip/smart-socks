1855bc7cf48ffadef71a0dcdab01b7a6ce1afe60
ModEngineer <43882205+ModEngineer@users.noreply.github.com>
2026-02-12 18:58:41 +0200

    Revert "made server synchronous"
    This reverts commit 71e0fd5e411c773d6671ab5a5d529bb88fe7b19a.

--- sock_boss/src/sock_boss.cpp
+++ sock_boss/src/sock_boss.cpp
@@ -1,11 +1,8 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <ESPmDNS.h>
+#include <ESPAsyncWebServer.h>
 #include "secrets.h"
-#include <WifiServer.h>
-#include <ArduinoJson.hpp>
-
-using namespace ArduinoJson;
 
 void init_wifi() {
   WiFi.mode(WIFI_MODE_STA);
@@ -25,10 +22,16 @@ void init_wifi() {
   }
 }
 
+static AsyncWebServer server(80);
+
 JsonDocument result;
-WiFiServer server(80);
+std::string text;
 
 void init_site() {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+
+    request->send(200, "text/html", (const uint8_t *)text.data(), text.size());
+  });
   server.begin();
 }
 
@@ -50,8 +53,8 @@ void loop() {
   int ball = analogRead(A9);
   result["heel"] = heel;
   result["ball"] = ball;
-  serializeJson(result, server);
-  /*   int spaces = value / 52;
+  serializeJson(result, text);
+/*   int spaces = value / 52;
 
   for (int i = 0; i<spaces; i++) {
     Serial.print(" ");
