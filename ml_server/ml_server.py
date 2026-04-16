7e8b7ebbed65d4eb13b11c7c3867b1e6e3208536
modengineer <modengineer@outlook.com>
2026-04-16 17:13:06 +0300

    implemented rudimentary ML

--- ml_server/ml_server.py
+++ ml_server/ml_server.py
@@ -5,159 +5,12 @@ import requests
 import time
 from io import StringIO
 from consts import *
+import create_model
+from ingestion import *
 
+output_mapping, model = create_model.create_model("../datasets")
 
-def r2a(other: float, resistances: np.ndarray) -> CubicSpline:
-    resistances[:, 1] /= resistances[:, 1] + other
-    return CubicSpline(
-        resistances[:, 1], resistances[:, 0], bc_type="natural", extrapolate=True
-    )
-
-
-# kg: resistance as measured from the hydraulic press
-# units aren't too important; all fed into ML model, just want data to be linear-ish
-linearization_data = {
-    # sensor 5
-    "heel1": r2a(
-        1000,
-        np.array(
-            [
-                [80.5 - 0.692, 380],
-                [72.4 - 0.692, 580],
-                [60.0 - 0.692, 710],
-                [50.9 - 0.692, 900],
-                [40.7 - 0.692, 1600],
-                [31.5 - 0.692, 3400],
-                [20.7 - 0.692, 9400],
-                [11.5 - 0.692, 15200],
-                [0.081, 31000],
-                [0, 500000],
-            ]
-        ),
-    ),
-    # sensor 1
-    "ball11": r2a(
-        100,
-        np.array(
-            [
-                [81.1 - 1.02, 92],
-                [70.8 - 1.02, 104],
-                [61.4 - 1.02, 123],
-                [52.0 - 1.02, 141],
-                [41.2 - 1.02, 171],
-                [31.1 - 1.02, 210],
-                [21.0 - 1.02, 280],
-                [11.2 - 1.02, 366],
-                [0.081, 16000],
-                [0, 1000000],
-            ]
-        ),
-    ),
-    # sensor 2
-    "ball12": r2a(
-        100,
-        np.array(
-            [
-                [81.0 - 1.02, 46],
-                [70.8 - 1.02, 52],
-                [61.2 - 1.02, 61],
-                [50.9 - 1.02, 72],
-                [41.5 - 1.02, 95],
-                [31.5 - 1.02, 132],
-                [21.1 - 1.02, 225],
-                [11.2 - 1.02, 351],
-                [0.081, 18000],
-                [0, 500000],
-            ]
-        ),
-    ),
-    # sensor 4
-    "heel2": r2a(
-        10000,
-        np.array(
-            [
-                [81.0 - 0.692, 1000],
-                [71.0 - 0.692, 2000],
-                [62.1 - 0.692, 3000],
-                [50.6 - 0.692, 5000],
-                [41.1 - 0.692, 12000],
-                [33.3 - 0.692, 14000],
-                [22.0 - 0.692, 38000],
-                [12.4 - 0.692, 43000],
-                [0.081, 150000],
-                [0, 2000000],
-            ]
-        ),
-    ),
-    # sensor 6
-    "ball21": r2a(
-        2200,
-        np.array(
-            [
-                [81.1 - 0.692, 430],
-                [69.9 - 0.692, 750],
-                [60.1 - 0.692, 1000],
-                [50.8 - 0.692, 1900],
-                [40.9 - 0.692, 3800],
-                [31.4 - 0.692, 8300],
-                [22.1 - 0.692, 8900],
-                [9.9 - 0.692, 11800],
-                [0.081, 26000],
-                [0, 500000],
-            ]
-        ),
-    ),
-    # sensor 3
-    "ball22": r2a(
-        220,
-        np.array(
-            [
-                [82.0 - 1.02, 103],
-                [72.2 - 1.02, 130],
-                [62.3 - 1.02, 140],
-                [51.9 - 1.02, 230],
-                [41.2 - 1.02, 310],
-                [31.1 - 1.02, 417],
-                [21.9 - 1.02, 1810],
-                [11.6 - 1.02, 15000],
-                [0.081, 17000],
-                [0, 500000],
-            ]
-        ),
-    ),
-}
-
-
-def linearize(df: pd.DataFrame):
-    for col in df.columns:
-        if col not in linearization_data:
-            continue
-        df[col] = linearization_data[col](df[col])
-
-
-for ary in linearization_data:
-    pass
-
-
-def max_fft(s: np.ndarray):
-    mean = s.mean()
-    freqs = np.fft.rfftfreq(len(s), 1 / SAMPLE_RATE)
-    ffts = np.abs(np.fft.rfft(s - mean))
-    i = ffts.argmax()
-    return (freqs[i], ffts[i], mean)
-
-
-def runml(df: pd.DataFrame):
-    max_ffts = {}
-    for col in df.columns:
-        if col == "time":
-            continue
-        res = max_fft(df[col].values)
-        max_ffts[col + "freq"] = "%.2f" % res[0]
-        max_ffts[col + "mag"] = "%.2f" % res[1]
-        max_ffts[col + "mean"] = "%.2f" % res[2]
-    return max_ffts
-
+pad_len = max(len(o) + 2 for o in output_mapping)
 
 def webserver():
     s = requests.session()
@@ -166,14 +19,18 @@ def webserver():
         new_data = pd.read_json(
             StringIO('{"0": ' + s.get("http://sock_boss.local").text + "}"),
             orient="index",
-        )
+        )[input_cols]
         linearize(new_data)
         data: pd.DataFrame = pd.concat([data, new_data])
         data = data.tail(WINDOW_SIZE)
-        ml = runml(data)
-        print("")
-        for k, v in ml.items():
-            print(k, v)
+        #ml = runml(data)
+        #print("")
+        # for k, v in ml.items():
+            # print(k, v)
+        #for k, v in new_data.to_dict('records')[0].items():
+        #    print(k,v)
+        ml_result = model([ml_inputs(data)])
+        print("\n".join(f"{k:<{pad_len}}{v}" for k, v in model([ml_inputs(data)]).items()), end="\n\n")
         time.sleep(1 / SAMPLE_RATE)
 
 
--- ml_server/record_raw.py
+++ ml_server/record_raw.py
@@ -3,9 +3,12 @@ import time
 import requests
 import pandas as pd
 from consts import *
+import sys
 
-
-def read_raw(target: str):
+def read_raw(target: str, delay: int):
+    for i in range(delay,0,-1):
+        print(i)
+        time.sleep(1)
     s = requests.session()
     data = pd.DataFrame()
     try:
@@ -14,7 +17,10 @@ def read_raw(target: str):
                 StringIO('{"0": ' + s.get("http://sock_boss.local").text + "}"),
                 orient="index",
             )
-            data = pd.concat([data, new_data])
+            data = pd.concat([data, new_data], ignore_index=True)
+            print(new_data)
             time.sleep(1 / SAMPLE_RATE)
     except:
         data.to_csv(target)
+        
+read_raw(sys.argv[1],2)
